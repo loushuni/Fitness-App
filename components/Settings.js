ea77abbf8d8b2bd60faf26e1a12306121894fd0c
Shuni Lou <loushuni@gmail.com>
2020-01-04 05:44:28 -0800

    added buttons on Settings page1 and changed styling

--- components/Settings.js
+++ components/Settings.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Login extends Component {
-    render () {
+    render() {
         return (
             <View>
-                <Text>Settings</Text>
+                <Text style={styles.title}>Select Gender</Text>
+                <TouchableOpacity >
+                    <Text style={styles.text}>Female</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.text}>Male</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.text}>Non-Binary</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "black",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 25,
+        marginTop: 50,
+        marginBottom: 250
+    },
+    text: {
+        fontSize: 16,
+        padding: 25,
+        borderColor: 'white',
+        backgroundColor: '#fff',
+        color: 'black',
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center'
+    }
+});
