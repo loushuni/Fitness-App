a9a55d16486ccf0d2115ddd1c52d083b30df753f
Shuni Lou <loushuni@gmail.com>
2020-01-06 16:29:20 -0800

    added image to Login page

--- components/Login.js
+++ components/Login.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { TextInput, View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { TextInput, View, StyleSheet, TouchableOpacity, Text, ImageBackground } from 'react-native';
 
 export default class Login extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.inputcontainer}>
-                    <Text style={styles.title}>Fitness App</Text>
-                    <TextInput style={styles.input} placeholder="Username" />
-                    <TextInput style={styles.input} placeholder="Password" />
-                </View>
-                <View>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttontext}>Login</Text>
-                    </TouchableOpacity>
-                </View>
+                <ImageBackground source={require('../assets/7.png')} style={styles.backgroundImage}>
+                    <View style={styles.inputcontainer}>
+                        <Text style={styles.title}>Fitness App</Text>
+                        <TextInput style={styles.input} placeholder="Username" />
+                        <TextInput style={styles.input} placeholder="Password" />
+                    </View>
+                    <View>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttontext}>Login</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
@@ -40,7 +42,7 @@ const styles = StyleSheet.create({
     },
     input: {
         fontSize: 18,
-        paddingHorizontal:10,
+        paddingHorizontal: 10,
         height: 40,
         backgroundColor: 'rgba(225,225,225,1)',
         marginBottom: 10,
@@ -60,5 +62,9 @@ const styles = StyleSheet.create({
         width: '100%',
         color: '#fff',
         fontWeight: '700',
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%'
     }
 });
