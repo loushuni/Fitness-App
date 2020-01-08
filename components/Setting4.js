11a7b9e2b138c81810e44725dc2972196e80804a
Shuni Lou <loushuni@gmail.com>
2020-01-07 21:38:21 -0800

    added styling, image and text to Setting4

--- components/Setting4.js
+++ components/Setting4.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default class Setting4 extends Component {
-    render () {
+    render() {
         return (
-            <View>
-                <Text></Text>
+            <View style={styles.container}>
+                <ImageBackground source={require('../assets/9.png')} style={styles.backgroundImage}>
+                    <Text style={styles.title}>Height</Text>
+                </ImageBackground>
+                <View style={styles.picker}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttontext}>Continue</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "white",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 27,
+        marginTop: 50,
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+        flex: 1.3
+    },
+    picker: {
+        flex: 0.7
+    },
+    button: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#333333',
+        paddingTop: 18
+    },
+    buttontext: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    container: {
+        flex: 1
+    }
+}); 
