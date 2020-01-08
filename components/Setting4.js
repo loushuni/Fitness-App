f6c25158669e0931a6918307f28f92b1839a184d
Shuni Lou <loushuni@gmail.com>
2020-01-08 03:48:36 -0800

    added picker on Setting4 and changed styling

--- components/Setting4.js
+++ components/Setting4.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground, Picker } from 'react-native';
 
 export default class Setting4 extends Component {
+    state = { user: '' }
+
+    updateUser = (user) => {
+        this.setState({ user: user })
+    }
+
     render() {
+        const array = ["130", "131", "132", "133", "134", "135", "136"];
+
         return (
-            <View style={styles.container}>
+            <View style={styles.container1}>
                 <ImageBackground source={require('../assets/9.png')} style={styles.backgroundImage}>
                     <Text style={styles.title}>Height</Text>
                 </ImageBackground>
-                <View style={styles.picker}>
+                <View style={styles.container2}>
                     <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Setting5')}>
-                        <Text style={styles.buttontext}>Continue</Text>
+                        <Text style={styles.buttonText}>Continue</Text>
                     </TouchableOpacity>
+                    <View style={styles.pickerView}>
+                        <Picker selectedValue={this.state.user} onValueChange={this.updateUser} style={styles.picker}>
+                            {array.map((item, index) => {
+                                return (< Picker.Item label={item} value={index} key={index} />);
+                            })}
+                        </Picker>
+                        <Picker selectedValue={this.state.user} onValueChange={this.updateUser} style={styles.picker}>
+                            < Picker.Item label="cm" value="cm" />
+                            < Picker.Item label="inch" value="inch" />
+                        </Picker>
+                    </View>
+
                 </View>
             </View>
         );
@@ -31,7 +51,10 @@ const styles = StyleSheet.create({
         height: '100%',
         flex: 1.3
     },
-    picker: {
+    container1: {
+        flex: 1
+    },
+    container2: {
         flex: 0.7
     },
     button: {
@@ -40,13 +63,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#333333',
         paddingTop: 18
     },
-    buttontext: {
+    buttonText: {
         color: 'white',
         fontSize: 18,
         textAlign: 'center',
         fontWeight: 'bold',
     },
-    container: {
-        flex: 1
+    pickerView: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    picker: {
+        flex: 0.5
     }
 }); 
