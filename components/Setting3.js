f54bf97a61ae9c4dd652bbc838ddbe0db7cd5888
Shuni Lou <loushuni@gmail.com>
2020-01-07 16:57:32 -0800

    added background image to Setting3

--- components/Setting3.js
+++ components/Setting3.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View, Picker } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 
 export default class Setting3 extends Component {
     render() {
         return (
             <View>
-                <Text>Weight</Text>
+                <ImageBackground source={require('../assets/8.png')} style={styles.backgroundImage}>
+                    <Text style={styles.title}>Date of Birth</Text>
+                </ImageBackground>
+                
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "white",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 27,
+        marginTop: 50,
+        marginBottom: 200
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '80%'
+    }
+});
