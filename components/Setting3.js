05120a75f59db33eb759826db26c03dd56254d2c
Shuni Lou <loushuni@gmail.com>
2020-01-07 17:11:43 -0800

    added date picker to Setting3 page

--- components/Setting3.js
+++ components/Setting3.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import { View, Button, StyleSheet, ImageBackground, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class Setting3 extends Component {
+    state = {
+        date: new Date('2020-01-07'),
+    }
+
+    setDate = (event, date) => {
+        date = date || this.state.date;
+
+        this.setState({
+            date
+        });
+    }
+
     render() {
+        const { date } = this.state;
+
         return (
             <View>
                 <ImageBackground source={require('../assets/8.png')} style={styles.backgroundImage}>
                     <Text style={styles.title}>Date of Birth</Text>
                 </ImageBackground>
-                
+                <Button onPress={this.datepicker} title="Show date picker!" />
+                <DateTimePicker
+                    value={date}
+                    display="default"
+                    onChange={this.setDate}
+                />
             </View>
         );
     }
@@ -25,6 +45,6 @@ const styles = StyleSheet.create({
     },
     backgroundImage: {
         width: '100%',
-        height: '80%'
+        height: '75%'
     }
-});
+}); 
