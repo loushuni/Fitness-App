2cbbd7d07cf561efd54fe69ccbd2e226a4c5d3d8
Shuni Lou <loushuni@gmail.com>
2020-01-13 03:01:02 -0800

    changed Login page styling to ui kitten style

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { TextInput, View, StyleSheet, TouchableOpacity, Text, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
+import { Text, Button, Input } from '@ui-kitten/components';
 
 export default class Login extends Component {
     render() {
@@ -7,11 +8,13 @@ export default class Login extends Component {
             <View style={styles.container}>
                 <ImageBackground source={require('../assets/7.png')} style={styles.backgroundImage}>
                     <Text style={styles.title}>Fitness App</Text>
-                    <TextInput style={styles.input} placeholder="Username" />
-                    <TextInput style={styles.input} placeholder="Password" />
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttontext}>Login</Text>
-                    </TouchableOpacity>
+                    <Input style={styles.input} placeholder="Username" />
+                    <Input style={styles.input} placeholder="Password" />
+                    <Button
+                        style={styles.button}
+                        textStyle={styles.buttonText}>
+                        Login
+                    </Button>
                 </ImageBackground>
             </View>
         );
@@ -34,10 +37,8 @@ const styles = StyleSheet.create({
     input: {
         fontSize: 18,
         height: 40,
-        backgroundColor: 'rgba(225,225,225,1)',
         marginBottom: 10,
         padding: 10,
-        color: '#fff',
         textAlign: 'center',
         width: '50%',
         alignSelf: 'center'
@@ -45,17 +46,16 @@ const styles = StyleSheet.create({
     button: {
         padding: 15,
         width: '45%',
-        backgroundColor: '#2980b6',
         paddingVertical: 15,
         marginTop: 20,
         alignSelf: 'center'
     },
-    buttontext: {
+    buttonText: {
         fontSize: 18,
         textAlign: 'center',
         width: '100%',
-        color: '#fff',
         fontWeight: '700',
+        padding: 3
     },
     backgroundImage: {
         width: '100%',
