ee06475e7b95984c8e4158716f72b3ede5f32269
Shuni Lou <loushuni@gmail.com>
2020-01-24 23:14:42 -0800

    modified BMI file and changed styling

--- components/BMI.js
+++ components/BMI.js
@@ -7,30 +7,47 @@ const BMI = () => {
 
     const [selectedOption, setSelectedOption] = React.useState(null);
 
+    const [weight, setWeight] = React.useState('');
+
+    const [height, setHeight] = React.useState('');
+
+   // let res = `${weight} + ${1}`;
+   let res = weight;
+
     return (
         <Layout style={styles.container}>
             <Text style={styles.title}>BMI Calculator</Text>
             <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ marginRight: 10 }}>Weight:</Text>
-                <Input placeholder='Enter Your Weight' style={styles.input} />
+                <Text style={{ marginRight: 10, marginTop: 8 }}>Weight:</Text>
+                <Input
+                    placeholder='Enter Your Weight'
+                    style={styles.input}
+                    value={weight}
+                    onChangeText={setWeight} />
                 <Select
                     data={weightData}
                     selectedOption={selectedOption}
                     onSelect={setSelectedOption}
+                    style={{width: '28%'}}
                 />
             </Layout>
             <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ marginRight: 10 }}>Height:</Text>
-                <Input placeholder='Enter Your Height' style={styles.input} />
+                <Text style={{ marginRight: 10, marginTop: 8 }}>Height:</Text>
+                <Input
+                    placeholder='Enter Your Height'
+                    style={styles.input}
+                    value={height}
+                    onChangeText={setHeight} />
                 <Select
                     data={heightData}
                     selectedOption={selectedOption}
                     onSelect={setSelectedOption}
+                    style={{width: '28%'}}
                 />
             </Layout>
             <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Button style={styles.button}>Calculate BMI</Button>
-                <Input placeholder='' style={{ width: '25%' }} />
+                <Text style={{ marginRight: 10, marginTop: 10, fontWeight: 'bold' }}>Your BMI:</Text>
+                <Input placeholder={res} style={{ width: '30%', marginTop: 3 }} />
             </Layout>
         </Layout>
     );
@@ -66,11 +83,9 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     input: {
-        width: '45%',
+        width: '40%',
         paddingRight: 10
     }
 })
 
 export default BMI;
-
-
