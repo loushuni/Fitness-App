0ab7321cd3b3e2169c122c23616c4b4884bd9e84
Shuni Lou <loushuni@gmail.com>
2020-01-24 20:59:47 -0800

    added unit select on BMI

--- components/BMI.js
+++ components/BMI.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Layout, Text, Input, Button } from '@ui-kitten/components';
+import { Layout, Text, Input, Button, Select } from '@ui-kitten/components';
 
-export default class BMI extends React.Component {
-    render() {
-        return (
-            <Layout style={styles.container}>
-                <Text style={styles.title}>BMI Calculator</Text>
-                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                    <Text style={{marginRight: 10}}>Weight:</Text>
-                    <Input placeholder='Enter Your Weight' style={styles.input} />
-                </Layout>
-                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                    <Text style={{marginRight: 10}}>Height:</Text>
-                    <Input placeholder='Enter Your Height' style={styles.input} />
-                </Layout>
-                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                    <Button style={styles.button}>Calculate BMI</Button>
-                    <Input placeholder='' style={{width: '25%'}}/>
-                </Layout>
+
+const BMI = () => {
+
+    const [selectedOption, setSelectedOption] = React.useState(null);
+
+    return (
+        <Layout style={styles.container}>
+            <Text style={styles.title}>BMI Calculator</Text>
+            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                <Text style={{ marginRight: 10 }}>Weight:</Text>
+                <Input placeholder='Enter Your Weight' style={styles.input} />
+                <Select
+                    data={weightData}
+                    selectedOption={selectedOption}
+                    onSelect={setSelectedOption}
+                />
             </Layout>
-        );
-    }
+            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                <Text style={{ marginRight: 10 }}>Height:</Text>
+                <Input placeholder='Enter Your Height' style={styles.input} />
+                <Select
+                    data={heightData}
+                    selectedOption={selectedOption}
+                    onSelect={setSelectedOption}
+                />
+            </Layout>
+            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                <Button style={styles.button}>Calculate BMI</Button>
+                <Input placeholder='' style={{ width: '25%' }} />
+            </Layout>
+        </Layout>
+    );
+
 }
+
+const weightData = [
+    { text: 'kg' },
+    { text: 'lb' }
+];
+
+const heightData = [
+    { text: 'cm' },
+    { text: 'inch' }
+];
 
 const styles = StyleSheet.create({
     container: {
@@ -43,6 +66,55 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     input: {
-        width: '48%'
+        width: '45%',
+        paddingRight: 10
     }
 })
+
+export default BMI;
+
+
+
+/*
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout, Select } from '@ui-kitten/components';
+
+const data1 = [
+    { text: 'kg' },
+    { text: 'lb' }
+];
+
+const data2 = [
+    { text: 'cm' },
+    { text: 'inch' }
+];
+
+const BMI = () => {
+
+    const [selectedOption, setSelectedOption] = React.useState(null);
+
+    return (
+        <Layout style={styles.container}>
+            <Select
+                data={data1}
+                selectedOption={selectedOption}
+                onSelect={setSelectedOption}
+            />
+            <Select
+                data={data2}
+                selectedOption={selectedOption}
+                onSelect={setSelectedOption}
+            />
+        </Layout>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+export default BMI;
+*/
