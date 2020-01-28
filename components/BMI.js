64337af0c45d68cba672aae93f325342c57f6fe0
Shuni Lou <loushuni@gmail.com>
2020-01-27 17:42:48 -0800

    commented out select components and changed styling in BMI file

--- components/BMI.js
+++ components/BMI.js
@@ -5,9 +5,9 @@ import { Layout, Text, Input, Button, Select } from '@ui-kitten/components';
 
 const BMI = () => {
 
-    const [selectedOption1, setSelectedOption1] = React.useState(null);
+    // const [selectedOption1, setSelectedOption1] = React.useState(null);
 
-    const [selectedOption2, setSelectedOption2] = React.useState(null);
+    // const [selectedOption2, setSelectedOption2] = React.useState(null);
 
     const [weight, setWeight] = React.useState('');
 
@@ -15,61 +15,59 @@ const BMI = () => {
 
     let res = null;
 
-    if (selectedOption1 == "kg" && selectedOption2 == "cm") {
-        res = weight;
-    } else if (selectedOption1 === 'lb' && selectedOption2 === 'inch') {
-        res = height;
-    }
-
     return (
         <Layout style={styles.container}>
             <Text style={styles.title}>BMI Calculator</Text>
-            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ marginRight: 10, marginTop: 8 }}>Weight:</Text>
-                <Input
-                    placeholder='Enter Your Weight'
-                    style={styles.input}
-                    value={weight}
-                    onChangeText={setWeight} />
-                <Select
+            <Layout style={{flexDirection: 'column', marginLeft: 50 }}>
+                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                    <Text style={{ marginRight: 10, marginTop: 8 }}>Weight:</Text>
+                    <Input
+                        placeholder='Enter Your Weight'
+                        style={styles.input}
+                        value={weight}
+                        onChangeText={setWeight} />
+                    {/* <Select
                     data={weightData}
                     selectedOption={selectedOption1}
                     onSelect={setSelectedOption1}
                     style={{ width: '28%' }}
-                />
-            </Layout>
-            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ marginRight: 10, marginTop: 8 }}>Height:</Text>
-                <Input
-                    placeholder='Enter Your Height'
-                    style={styles.input}
-                    value={height}
-                    onChangeText={setHeight} />
-                <Select
+                /> */}
+                    <Text style={{ paddingTop: 10 }}>lb</Text>
+                </Layout>
+                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                    <Text style={{ marginRight: 10, marginTop: 8 }}>Height:</Text>
+                    <Input
+                        placeholder='Enter Your Height'
+                        style={styles.input}
+                        value={height}
+                        onChangeText={setHeight} />
+                    {/* <Select
                     data={heightData}
                     selectedOption={selectedOption2}
                     onSelect={setSelectedOption2}
                     style={{ width: '28%' }}
-                />
-            </Layout>
-            <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ marginRight: 10, marginTop: 10, fontWeight: 'bold' }}>Your BMI:</Text>
-                <Text style={styles.result}>{height}</Text>
+                /> */}
+                    <Text style={{ paddingTop: 10 }}>inch</Text>
+                </Layout>
+                <Layout style={{ flexDirection: 'row', marginBottom: 10 }}>
+                    <Text style={{ marginRight: 10, marginTop: 10, fontWeight: 'bold' }}>Your BMI:</Text>
+                    <Text style={styles.result}>{height}</Text>
+                </Layout>
             </Layout>
         </Layout>
     );
 
 }
 
-const weightData = [
-    { text: 'kg' },
-    { text: 'lb' }
-];
+// const weightData = [
+//     { text: 'kg' },
+//     { text: 'lb' }
+// ];
 
-const heightData = [
-    { text: 'cm' },
-    { text: 'inch' }
-];
+// const heightData = [
+//     { text: 'cm' },
+//     { text: 'inch' }
+// ];
 
 const styles = StyleSheet.create({
     container: {
@@ -90,16 +88,17 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     input: {
-        width: '40%',
+        width: '55%',
         paddingRight: 10
     },
     result: {
         borderWidth: 1,
         borderColor: '#101426',
         paddingHorizontal: 40,
-        paddingTop: 8,
-        paddingBottom: 8,
-        backgroundColor: '#1A2138'
+        paddingTop: 11,
+        paddingBottom: 11,
+        backgroundColor: '#1A2138',
+        width: '35%'
     }
 })
 
