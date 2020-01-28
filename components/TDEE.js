14f5faf011899a7d53d60254c3204c11fba228db
Shuni Lou <loushuni@gmail.com>
2020-01-27 21:54:48 -0800

    added gender radio group in TDEE

--- components/TDEE.js
+++ components/TDEE.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Layout, Text, Input, Button, Select } from '@ui-kitten/components';
+import { Layout, Text, Input, Button, Select, Radio, RadioGroup } from '@ui-kitten/components';
 
 const TDEE = () => {
 
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+    const onCheckedChange = (index) => {
+        setSelectedIndex(index);
+    };
+
+    return (
+        <Layout style={styles.container}>
+            <Text style={styles.title}>TDEE Calculator</Text>
+            <Layout>
+                <Layout style={{ flexDirection: 'row' }}>
+                    <Text>Gender</Text>
+                    <RadioGroup
+                        selectedIndex={selectedIndex}
+                        onChange={onCheckedChange}
+                        style={{ flexDirection: 'row' }}>
+                        <Radio text='Male' />
+                        <Radio text='Female' />
+                    </RadioGroup>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        color: "white",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 27,
+        marginTop: 80,
+        marginBottom: 200,
+        paddingTop: 10
+    },
+});
 
 export default TDEE;
