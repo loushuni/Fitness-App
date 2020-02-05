8687e7c12f0a8fe08a179c9e2aceda000c49f8c4
Shuni Lou <loushuni@gmail.com>
2020-02-05 03:15:57 -0800

    added title and two inputs

--- components/OneRM.js
+++ components/OneRM.js
@@ -3,11 +3,40 @@ import { StyleSheet } from 'react-native';
 import { Layout, Text, Input, Button, Select } from '@ui-kitten/components';
 
 const OneRM = () => {
+    const [lift, setLift] = React.useState('');
+
     return (
-        <Layout>
+        <Layout style={styles.container}>
+            <Text style={styles.title}>1RM Calculator</Text>
+            <Layout>
+                <Layout style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{marginRight: 10}}>Lift:</Text>
+                    <Input placeholder='Weight Lifted' value={lift} onChangeText={setLift} />
+                </Layout>
+                <Layout style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{marginRight: 10}}>Repetitions:</Text>
+                    <Input placeholder='Repetitions' />
+                </Layout>
+            </Layout>
 
         </Layout>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        color: "white",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 27,
+        marginTop: 80,
+        marginBottom: 100,
+        paddingTop: 10
+    },
+});
 
 export default OneRM;
