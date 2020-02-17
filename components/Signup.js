b066cb7f0dfb607174ef1699fc567fdbd096401c
Shuni Lou <loushuni@gmail.com>
2020-02-17 05:29:26 -0800

    installed GoogleSignIn library and changed signin handler

--- components/Signup.js
+++ components/Signup.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, StyleSheet, ImageBackground, Alert } from 'react-native';
 import { Text, Button, Icon } from '@ui-kitten/components';
 import * as firebase from 'firebase';
+//import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
+import { GoogleSignin } from 'react-native-google-signin';
 
 export default class Signup extends Component {
     render() {
@@ -25,18 +27,100 @@ export default class Signup extends Component {
     }
 }
 
-const pressed = () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-}
+// const login = () => {
+//     GoogleSignin.hasPlayServices({ autoResolve: true })
+//         .then(() => {
+//             GoogleSignin.configure(KEYS)
+//                 .then(() => {
+//                     GoogleSignin.signIn()
+//                         .then(onGoogleLoginSuccess)
+//                         .catch(error => { })
+//                         .done();
+//                 });
+//         })
+//         .catch((err) => {
+//             console.log("Play services error", err.code, err.message);
+//         })
+// }
+
+
+// const onGoogleLoginSuccess = (user) => {
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//     var token = user.idToken;
+//     const credential = provider.credential(token);
+//     firebase.auth().signInWithCredential(credential)
+//         .then((data) => {
+//             console.log('SUCCESS', data);
+//         })
+//         .catch((error) => {
+//             console.log('ERROR', error);
+//         });
+// }
+// 
+
+
+// GoogleSignin.configure({}).then(() => {
+//     GoogleSignin.hasPlayServices({ autoResolve: true })
+//       .then(() => {
+//         GoogleSignin.signIn()
+//           .then(user => {
+//             console.log(user);
+
+//             const credential = firebase.auth.GoogleAuthProvider.credential(
+//               user.idToken,
+//               user.accessToken
+//             );
+
+//             firebase
+//               .auth()
+//               .signInWithCredential(credential)
+//               .then(user => {
+//                 console.log("user firebase ", user);
+//                 if (user._authObj.authenticated) {
+//                   // do you login action here
+//                   // dispatch({
+//                   //  type: LOGIN_SUCCESS,
+//                   //  payload: { ...user._user, loggedIn: true }
+//                   //});
+//                 }
+//               });
+//           })
+//           .catch(err => {
+//             console.log("WRONG SIGNIN", err);
+//           })
+//           .done();
+//       })
+//       .catch(err => {
+//         console.log("Play services error", err.code, err.message);
+//       });
+//  });
+
+async function googleLogin() {
+    try {
+      // add any configuration settings here:
+      await GoogleSignin.configure();
+  
+      const data = await GoogleSignin.signIn();
+  
+      // create a new firebase credential with the token
+      const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
+      // login with credential
+      const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
+  
+      console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
 
 const GoogleIcon = (style) => (
     <Icon name='google' {...style} />
 );
 
 const GoogleButton = () => (
-    <Button icon={GoogleIcon} style={styles.button2} onPress={pressed}>Continue With Google</Button>
-  );
+    <Button icon={GoogleIcon} style={styles.button2} onPress={googleLogin}>Continue With Google</Button>
+);
 
 const AppleIcon = (style) => (
     <Icon name='apple' {...style} />
@@ -44,7 +128,7 @@ const AppleIcon = (style) => (
 
 const AppleButton = () => (
     <Button icon={AppleIcon} style={styles.button2}>Continue With Apple</Button>
-  );
+);
 
 const FacebookIcon = (style) => (
     <Icon name='facebook-f' {...style} />
@@ -52,7 +136,7 @@ const FacebookIcon = (style) => (
 
 const FacebookButton = () => (
     <Button icon={FacebookIcon} style={styles.button3}>Sign Up with Facebook</Button>
-  );
+);
 
 const styles = StyleSheet.create({
     container: {
