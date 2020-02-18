00dadd631016719635fd46e33822fa94671358ae
Shuni Lou <loushuni@gmail.com>
2020-02-18 00:52:01 -0800

    commented out all google sign in code

--- components/Signup.js
+++ components/Signup.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, ImageBackground, Alert } from 'react-native';
 import { Text, Button, Icon } from '@ui-kitten/components';
-import * as firebase from 'firebase';
+//import * as firebase from 'firebase';
+//import * as Google from 'expo-google-app-auth';
 //import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
-import { GoogleSignin } from 'react-native-google-signin';
+//import { GoogleSignin } from 'react-native-google-signin';
 
 export default class Signup extends Component {
+    // signInWithGoogleAsync = async () => {
+    //     try {
+    //         const result = await Google.logInAsync({
+    //             //androidClientId: YOUR_CLIENT_ID_HERE,
+    //             behavior: 'web',
+    //             iosClientId: '257940969653-u7ds2nq93f098i6kou004k2csagu0ebt.apps.googleusercontent.com',
+    //             scopes: ['profile', 'email'],
+    //         });
+
+    //         if (result.type === 'success') {
+    //             return result.accessToken;
+    //         } else {
+    //             return { cancelled: true };
+    //         }
+    //     } catch (e) {
+    //         return { error: true };
+    //     }
+    // }
     render() {
         return (
             <View style={styles.container}>
@@ -95,23 +114,23 @@ export default class Signup extends Component {
 //       });
 //  });
 
-async function googleLogin() {
-    try {
-      // add any configuration settings here:
-      await GoogleSignin.configure();
-  
-      const data = await GoogleSignin.signIn();
-  
-      // create a new firebase credential with the token
-      const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
-      // login with credential
-      const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
-  
-      console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
-    } catch (e) {
-      console.error(e);
-    }
-  }
+// async function googleLogin() {
+//     try {
+//         // add any configuration settings here:
+//         await GoogleSignin.configure();
+
+//         const data = await GoogleSignin.signIn();
+
+//         // create a new firebase credential with the token
+//         const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
+//         // login with credential
+//         const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
+
+//         console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
+//     } catch (e) {
+//         console.error(e);
+//     }
+// }
 
 
 const GoogleIcon = (style) => (
@@ -119,7 +138,7 @@ const GoogleIcon = (style) => (
 );
 
 const GoogleButton = () => (
-    <Button icon={GoogleIcon} style={styles.button2} onPress={googleLogin}>Continue With Google</Button>
+    <Button icon={GoogleIcon} style={styles.button2} >Continue With Google</Button>
 );
 
 const AppleIcon = (style) => (
