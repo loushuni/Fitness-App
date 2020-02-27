2a7a4031eba548438f77be5c11bd335ec7e4d992
Shuni Lou <loushuni@gmail.com>
2020-02-27 01:43:01 -0800

    created a new file Calculators with all the calculator options

---
+++ components/Calculators.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, ImageBackground, Alert } from 'react-native';
+import { Text, Button, Input, Layout } from '@ui-kitten/components';
+
+const Calculators = () => {
+    return (
+        <Layout>
+            <Text>Calculators</Text>
+        </Layout>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    title: {
+
+    }
+})
+
+export default Calculators;
