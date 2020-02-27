1e4daf84491946fb3d983ba2b5b1ffd42cba2b42
Shuni Lou <loushuni@gmail.com>
2020-02-27 02:08:27 -0800

    changed navigation route to Calculators after login and added styling to Calculators

--- components/Calculators.js
+++ components/Calculators.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ImageBackground, Alert } from 'react-native';
-import { Text, Button, Input, Layout } from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
+import { Text, Button, Layout, Icon } from '@ui-kitten/components';
 
 const Calculators = () => {
     return (
-        <Layout>
-            <Text>Fitness Calculators</Text>
-            <View style={{ flexDirection: 'column', alignContent: 'flex-start', alignItems: 'flex-start' }}>
+        <Layout style={styles.container}>
+            <Text style={styles.title}>Fitness Calculators</Text>
+            <Layout style={{ flexDirection: 'column', alignContent: 'flex-start', alignItems: 'flex-start' }}>
                 <Button
                     style={styles.button}
                     textStyle={{ fontWeight: '500', marginRight: 245 }}
@@ -27,17 +27,38 @@ const Calculators = () => {
                     textStyle={{ fontWeight: '500', marginRight: 241 }}
                     icon={arrowIcon}
                     onPress={() => this.props.navigation.navigate('OneRM')}>1RM Calculator</Button>
-            </View>
+            </Layout>
         </Layout>
     );
 }
 
+const arrowIcon = (style) => (
+    <Icon name='angle-right' style={{ color: '#8F9BB3', paddingLeft: 10 }} />
+);
+
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1
     },
     title: {
-
+        color: "white",
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 27,
+        marginTop: 80,
+        marginBottom: 100,
+        paddingTop: 10
+    },
+    button: {
+        width: '100%',
+        borderWidth: 1,
+        height: 50,
+        flexDirection: 'row',
+        borderBottomWidth: 0,
+        backgroundColor: '#2E3A59',
+        borderColor: '#101426',
+        flexDirection: 'row-reverse',
+        justifyContent: 'flex-start'
     }
 })
 
