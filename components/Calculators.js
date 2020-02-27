bd815ec090bce09dd6674c9bf6c41432a68ccbe4
Shuni Lou <loushuni@gmail.com>
2020-02-27 01:54:55 -0800

    added all kinds of calculators into Calculators file

--- components/Calculators.js
+++ components/Calculators.js
@@ -6,6 +6,28 @@ const Calculators = () => {
     return (
         <Layout>
             <Text>Fitness Calculators</Text>
+            <View style={{ flexDirection: 'column', alignContent: 'flex-start', alignItems: 'flex-start' }}>
+                <Button
+                    style={styles.button}
+                    textStyle={{ fontWeight: '500', marginRight: 245 }}
+                    icon={arrowIcon}
+                    onPress={() => this.props.navigation.navigate('BMI')}>BMI Calculator</Button>
+                <Button
+                    style={styles.button}
+                    textStyle={{ fontWeight: '500', marginRight: 235 }}
+                    icon={arrowIcon}
+                    onPress={() => this.props.navigation.navigate('TDEE')}>TDEE Calculator</Button>
+                <Button
+                    style={styles.button}
+                    textStyle={{ fontWeight: '500', marginRight: 228 }}
+                    icon={arrowIcon}
+                    onPress={() => this.props.navigation.navigate('Macro')}>Macro Calculator</Button>
+                <Button
+                    style={styles.button}
+                    textStyle={{ fontWeight: '500', marginRight: 241 }}
+                    icon={arrowIcon}
+                    onPress={() => this.props.navigation.navigate('OneRM')}>1RM Calculator</Button>
+            </View>
         </Layout>
     );
 }
