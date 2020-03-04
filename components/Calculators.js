1c42deaf83e90626abf9c393178c20e4977424b8
Shuni Lou <loushuni@gmail.com>
2020-03-03 17:44:19 -0800

    changed styling, deleted scrollview

--- components/Calculators.js
+++ components/Calculators.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, ImageBackground, ScrollView, Alert } from 'react-native';
-import { Text, Button, Layout, Icon } from '@ui-kitten/components';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { Text, Button, Layout } from '@ui-kitten/components';
 
 export default class Calculators extends Component {
     render() {
         return (
             <Layout style={styles.container}>
                 <Text style={styles.title}>Fitness Calculators</Text>
-                <ScrollView>
-                    <Layout style={{ flex: 8 }} >
-                        <ImageBackground source={require('../assets/13.png')} style={styles.backgroundImage} >
-                            <Text style={styles.text} onPress={() => this.props.navigation.navigate('BMI')}>BMI Calculator</Text>
-                        </ImageBackground>
-                        <ImageBackground source={require('../assets/14.png')} style={styles.backgroundImage} >
-                            <Text style={styles.text} onPress={() => this.props.navigation.navigate('TDEE')}>TDEE Calculator</Text>
-                        </ImageBackground>
-                        <ImageBackground source={require('../assets/15.png')} style={styles.backgroundImage} >
-                            <Text style={styles.text} onPress={() => this.props.navigation.navigate('Macro')}>Macro Calculator</Text>
-                        </ImageBackground>
-                        <ImageBackground source={require('../assets/16.png')} style={styles.backgroundImage} >
-                            <Text style={styles.text} onPress={() => this.props.navigation.navigate('OneRM')}>1RM Calculator</Text>
-                        </ImageBackground>
-                    </Layout>
-                </ScrollView>
+                <ImageBackground source={require('../assets/13.png')} style={styles.backgroundImage} >
+                    <Text style={styles.text} onPress={() => this.props.navigation.navigate('BMI')}>BMI Calculator</Text>
+                </ImageBackground>
+                <ImageBackground source={require('../assets/14.png')} style={styles.backgroundImage} >
+                    <Text style={styles.text} onPress={() => this.props.navigation.navigate('TDEE')}>TDEE Calculator</Text>
+                </ImageBackground>
+                <ImageBackground source={require('../assets/15.png')} style={styles.backgroundImage} >
+                    <Text style={styles.text} onPress={() => this.props.navigation.navigate('Macro')}>Macro Calculator</Text>
+                </ImageBackground>
+                <ImageBackground source={require('../assets/16.png')} style={styles.backgroundImage} >
+                    <Text style={styles.text} onPress={() => this.props.navigation.navigate('OneRM')}>1RM Calculator</Text>
+                </ImageBackground>
                 <Button style={styles.profileButton} onPress={() => { this.props.navigation.navigate('Profile') }}>Go to Your Profile</Button>
             </Layout>
         );
     }
 }
-
-// const arrowIcon = (style) => (
-//     <Icon name='angle-right' style={{ color: '#8F9BB3', paddingLeft: 10 }} />
-// );
 
 const styles = StyleSheet.create({
     container: {
@@ -44,7 +36,8 @@ const styles = StyleSheet.create({
         fontSize: 27,
         marginTop: 80,
         marginBottom: 100,
-        paddingTop: 10
+        paddingTop: 10,
+        flex: 0.1
     },
     text: {
         fontSize: 22,
@@ -54,10 +47,9 @@ const styles = StyleSheet.create({
         width: '50%',
         alignSelf: 'center',
         marginBottom: 50,
-        //marginTop: 30
+        marginTop: 30
     },
     backgroundImage: {
-        height: '135%',
-        width: '100%'
+        flex: 0.3
     }
 })
